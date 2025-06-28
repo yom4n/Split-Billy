431ce6521fa4dca6abc0207d112dce92ad78f921
sparsh <sparshmcwan.07@gmail.com>
2025-06-28 12:07:24 +0530

    Removed unnecessary code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,9 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
+    react()
   ].filter(Boolean),
   resolve: {
     alias: {
